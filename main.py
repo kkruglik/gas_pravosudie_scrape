297d561097a5c26c40762df66229a169f63a7b0e
kkruglik <kirillkruglikov@outlook.com>
2022-07-13 09:28:59 +0300

    updated scraper, add more articles data

--- main.py
+++ main.py
@@ -36,13 +36,16 @@ if __name__ == '__main__':
     ]
     timeout = 100
 
-    for p in range(0, 86330, 1000):
+    for p in range(8000, 11000, 1000):
         first_page = p
         last_page = p + 1010
-        file_name = 'data_19_3_' + str(last_page)
+        file_name = 'data_207_' + str(last_page)
         data = defaultdict(list,{ k:[] for k in elem_for_search })
-        url = 'https://bsr.sudrf.ru/bigs/portal.html#%7B%22type%22:%22MULTIQUERY%22,%22multiqueryRequest%22:%7B%22queryRequests%22:%5B%7B%22type%22:%22Q%22,%22request%22:%22%7B%5C%22mode%5C%22:%5C%22EXTENDED%5C%22,%5C%22typeRequests%5C%22:%5B%7B%5C%22fieldRequests%5C%22:%5B%7B%5C%22name%5C%22:%5C%22case_user_doc_entry_date%5C%22,%5C%22operator%5C%22:%5C%22B%5C%22,%5C%22query%5C%22:%5C%222022-01-01T00:00:00%5C%22,%5C%22sQuery%5C%22:%5C%222022-07-01T00:00:00%5C%22,%5C%22fieldName%5C%22:%5C%22case_user_doc_entry_date%5C%22%7D,%7B%5C%22name%5C%22:%5C%22case_doc_instance%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%22%D0%9F%D0%B5%D1%80%D0%B2%D0%B0%D1%8F%20%D0%B8%D0%BD%D1%81%D1%82%D0%B0%D0%BD%D1%86%D0%B8%D1%8F%5C%22,%5C%22sQuery%5C%22:null%7D,%7B%5C%22name%5C%22:%5C%22case_common_parts_law_article%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%2219.3%5C%22,%5C%22sQuery%5C%22:null%7D,%7B%5C%22name%5C%22:%5C%22case_common_parts_law_article%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%22%D1%81%D1%82.%2019.3%5C%22,%5C%22sQuery%5C%22:null%7D%5D,%5C%22mode%5C%22:%5C%22AND%5C%22,%5C%22name%5C%22:%5C%22common%5C%22,%5C%22typesMode%5C%22:%5C%22AND%5C%22%7D%5D%7D%22,%22operator%22:%22AND%22,%22queryRequestRole%22:%22CATEGORIES%22%7D,%7B%22type%22:%22SQ%22,%22queryId%22:%221f074cdd-f56a-4089-b901-df7a05b3a5c8%22,%22operator%22:%22AND%22%7D%5D%7D,%22sorts%22:%5B%7B%22field%22:%22score%22,%22order%22:%22desc%22%7D%5D,%22simpleSearchFieldsBundle%22:null,%22groups%22:%5B%5D,%22noOrpho%22:false'
+        url = 'https://bsr.sudrf.ru/bigs/portal.html#%7B%22rows%22:10,%22uid%22:%22237dafd6-4889-4658-8cdc-8c298d264393%22,%22type%22:%22MULTIQUERY%22,%22multiqueryRequest%22:%7B%22queryRequests%22:%5B%7B%22type%22:%22Q%22,%22request%22:%22%7B%5C%22mode%5C%22:%5C%22EXTENDED%5C%22,%5C%22typeRequests%5C%22:%5B%7B%5C%22fieldRequests%5C%22:%5B%7B%5C%22name%5C%22:%5C%22u_case_user_article%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%22%D0%A1%D1%82%D0%B0%D1%82%D1%8C%D1%8F%20%20207%5C%22,%5C%22sQuery%5C%22:null%7D,%7B%5C%22name%5C%22:%5C%22u_case_user_article%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%22%D0%A1%D1%82%D0%B0%D1%82%D1%8C%D1%8F%20%20207.1%5C%22,%5C%22sQuery%5C%22:null%7D,%7B%5C%22name%5C%22:%5C%22u_case_user_article%5C%22,%5C%22operator%5C%22:%5C%22EX%5C%22,%5C%22query%5C%22:%5C%22%D0%A1%D1%82%D0%B0%D1%82%D1%8C%D1%8F%20%20207.2%5C%22,%5C%22sQuery%5C%22:null%7D%5D,%5C%22mode%5C%22:%5C%22AND%5C%22,%5C%22name%5C%22:%5C%22%D0%A3%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%B4%D0%B5%D0%BB%D0%B0%5C%22,%5C%22typesMode%5C%22:%5C%22AND%5C%22%7D%5D%7D%22,%22operator%22:%22AND%22,%22queryRequestRole%22:%22CATEGORIES%22%7D,%7B%22type%22:%22SQ%22,%22queryId%22:%227f9e8ff8-4bc8-46aa-bcbd-f2b3ed5f159f%22,%22operator%22:%22AND%22%7D%5D%7D,%22sorts%22:%5B%7B%22field%22:%22score%22,%22order%22:%22desc%22%7D%5D,%22simpleSearchFieldsBundle%22:null,%22groups%22:%5B%22%D0%A3%D0%B3%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D1%8B%D0%B5%20%D0%B4%D0%B5%D0%BB%D0%B0%22%5D,%22noOrpho%22:false,%22facet%22:%7B%22field%22:%5B%22type%22%5D%7D,%22facetLimit%22:21,%22additionalFields%22:%5B%22case_user_doc_number%22,%22case_user_document_type%22,%22case_common_parts_law_article%22,%22case_user_entry_date%22,%22case_user_doc_result_date%22,%22case_doc_subject_rf%22,%22case_user_doc_court%22,%22adm_case_user_name%22,%22case_user_doc_result%22%5D,%22hlFragSize%22:1000,%22groupLimit%22:3,%22woBoost%22:false'
         browser = webdriver.Chrome(ChromeDriverManager().install(), ) # options=chrome_options
-        failed_pages.append(scrape_sent(first_page, last_page, browser, url, elem_for_search, data, timeout, file_name))
+        failed_pages.append(
+            scrape_sent(first_page, last_page, browser, url, elem_for_search, data, timeout, file_name, 10070)
+            )
         browser.close()
-        to_pickle(failed_pages, 'failed_pages_19_3')
+        
+    to_pickle(failed_pages, 'failed_pages_207')
--- scrape_sentences.py
+++ scrape_sentences.py
@@ -15,20 +15,20 @@ from modules import to_pickle, scrape_elements
 from collections import defaultdict
 
 
-def scrape_sent(first_page, last_page, browser, url, elem_for_search, data, timeout, file_name):
+def scrape_sent(first_page, last_page, browser, url, elem_for_search, data, timeout, file_name, break_page):
     failed_pages = []
-    for page in tqdm(range(first_page, last_page, 10)): #11630
-        if page > 645070:
+    for page in tqdm(range(first_page, last_page, 10)): 
+        if page > break_page:
             break
         page_url = url + f',%22start%22:{page}%7D'
         browser.get(page_url)
-        # time.sleep(1)
+        time.sleep(1)
         
         if page == first_page:
             try:
                 WebDriverWait(browser, timeout).until(
                     lambda browser: browser.find_elements(By.XPATH, "//div[@class='bgs-result']"))
-                # time.sleep(1)
+                time.sleep(1)
 
                 WebDriverWait(browser, timeout).until(
                     EC.element_to_be_clickable((By.XPATH, "//a[@title='Таблицей']"))).click() 
@@ -43,10 +43,10 @@ def scrape_sent(first_page, last_page, browser, url, elem_for_search, data, time
         try:
             WebDriverWait(browser, timeout).until(
                     lambda browser: browser.find_elements(By.XPATH, '//div[@data-field="case_user_doc_number"]'))
-            # time.sleep(2)
+            time.sleep(2)
             table = browser.find_element_by_class_name('resultsList').find_element(By.TAG_NAME, "tbody") 
             scrape_elements(data, table, elem_for_search)
-            
+            to_pickle(data, file_name)
         except Exception as e:
             print(e, page)
             failed_pages.append(page)
